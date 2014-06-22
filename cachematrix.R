9ced61f3179ee123850a28dc2f4f1a69d0d341c7
alext1204 <mail.alex.t@gmail.com>
2014-06-22 14:45:21 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,70 @@
-## Put comments here that give an overall description of what your
-## functions do
+makeCacheMatrix <- function(x = matrix()) 
+{
 
-## Write a short comment describing this function
+####    creating a vector-object with 4 methods - set, get, setInverse and getInverse
+####    initialising inv
 
-makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
 
+
+####    set function sets the value of the matrix at first 
+####    setting inv, the value that contains the invese of the matrix as NULL 
+####    if it is called for the first time, NULL will be returned as inv value
+
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+
+
+####    get function returns the value of the function
+
+
+        get <- function() x
+
+
+####    sets the inverse to the inverse matrix variable, inv
+
+
+        setInverse <- function(invse) inv <<- invse
+
+####    returns the value of the inverse matrix variable, inv
+
+        getInverse <- function() inv
+
+####    creates a list with each of the names associated with the fns
+
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...) 
+{
+
+####    gets the inv from x using the getInverse function
+
+	inv <-x$getInverse()
+
+####    checks if inv is null ie called for first time. If not null, returns the inv
+####    with a comment so that you know that it is not calculating the value
+
+	if(!is.null(inv))
+	{
+                message("getting cached inverse")
+                return(inv)
+	}
+
+
+####    if null, gets the matrix in data and solves for inverse 
+	
+	data<-as.matrix(x$get())
+	inv <- solve(data)
+
+####    sets inverse in x
+
+	x$setInverse(inv)
+	inv
 }
